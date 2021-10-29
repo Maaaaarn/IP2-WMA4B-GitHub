3dd2188cf57a63b7535824720709cb2bd2c0fb72
Maaaaarn <91856387+Maaaaarn@users.noreply.github.com>
2021-10-29 15:57:42 +0800

    Update index.php

---
+++ index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DC Steaks & Burgers&mdash; </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,600i,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<!-- <div class="top-menu"> -->
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-center logo-wrap">
+						<div id="fh5co-logo"><a href="index.php">DC Steaks & Burgers<span>.</span></a></div>
+					</div>
+					
+					<div class="col-xs-12 text-center menu-1 menu-wrap">
+						<ul>
+							<li class="active"><a href="index.php">Home</a></li>
+							<li><a href="menu.html">Menu</a></li>
+							<li class="has-dropdown">
+								<a href="gallery.html">Gallery</a>
+					
+							<li><a href="about.html">About</a></li>
+							<li><a href="contact.html">Contact</a></li>
+							<li><a href="rate.html">Rate Us</a></li>
+							<li><a href="menu_checkout.php">Order Here</a></li>
+						</ul>
+					
+				</form>
+				</div>
+				
+			</div>
+		<!-- </div> -->
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover js-fullheight">  
+		<video src="images/burning.mp4" muted loop autoplay></video> 
+		
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-upper">
+					<div class="display-t js-fullheight">
+						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+							<div class="center"></div><h1>The Best <div> Grilled Steaks <em>&amp; </em>Burgers <div> in town </div></div></div></h1>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-about" class="fh5co-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-pull-4 img-wrap animate-box" data-animate-effect="fadeInLeft">
+					<img src="images/restaurant.jpg" height=500; width=800;>
+				</div>
+				<div class="col-md-5 col-md-push-1 animate-box">
+					<div class="section-heading">
+						<h2>The Restaurant</h2>
+						<p>Our restaurant's foundation is the teamwork and passion of our crew. They solidify every details to make every recipe tasty and remarkable. For them, it's not about the recipe itself, but how it leaves a unique signature on our customer's tastebuds. </p>
+						<p>	OPEN DAILY | 2PM-12MN </p>
+						<p><a href="https://www.google.com/maps/dir/15.4045338,120.6084826/15.1399494,120.58449/@15.2739863,120.4539364,11z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0" target="_blank" class="btn btn-primary btn-outline">Guide Map on how to find us</a></p>
+						<div class="col-md-5 col-md-push-1 animate-box">
+							<div class="section-heading">
+						
+								<p><a href=https://www.facebook.com/dcsteaksandburgers target="_blank" class="btn btn-primary btn-outline">VISIT OUR FACEBOOK PAGE</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-featured-menu" class="fh5co-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 fh5co-heading animate-box">
+					<h2>Specialties</h2>
+					<div class="row">
+						<div class="col-md-6">
+							<p>If you ever find it difficult to choose from our menu, here are the few specialties we can offer you.</p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item">
+						<img src="images/steaks.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Steaks</h3>
+						<span class="fh5co-price">Price starts @P219<sup></sup></span>
+						<p>Our steaks are made with 100% beef. Flame grilled, served hot and straight out from the griller, and that's what makes us unique. As you taste it, the smokey-flavor will definitely smack your tastebuds. It is best paired with softly mashed potato and coleslaw. </p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item margin_top">
+						<img src="images/monsterbbc.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Monster BBC</h3>
+						<span class="fh5co-price">@P399<sup></sup></span>
+						<p>Monster BBC is layered with 3 flame-grilled patties, 100% pure beef. Using an extender is not in our language so you get to enjoy the purest and meatiest every bits of our Monster BBC. In between the patties are filled with thick cheesy sauce </p>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block visible-xs-block"></div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item">
+						<img src="images/saladnachos.JPG" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Nachos and Salads</h3>
+						<span class="fh5co-price">P199 for Nachos, Salad starts @P139</span>
+						<p>Nachos and Salad is the best winning pair for us. 
+							You thought it's just a basic solo nachos and salad, but it's not.</p>
+							<p>Our nachos is set with pure ground beef, tomatoes, white onions, parsley and sprinkled with parmesan cheese on top. </p>
+							
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item margin_top">
+						<img src="images/pastas.jpg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Pastas</h3>
+						<span class="fh5co-price">Price starts @P139<sup></sup></span>
+						<p>Our Salad is tossed with white onions and croutons, sprinkled with parmesan cheese and sprayed with olive oil allover. Matching our Pesto Pasta,
+							 tossed with crushed basil and dressed with our secret sweet and sour cheese. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-featured-testimony" class="fh5co-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 fh5co-heading animate-box">
+					<h2>Testimony</h2>
+					<div class="row">
+						<div class="col-md-6">
+							<p> We believe and value our customers opinion, that's why our room for improvements are filled with challenges that teaches us how to serve and work better for our customers </p>
+							<p> Customers feedback is one of our fundamental basis to further grow as a team and make our service unforgettable.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-5 animate-box img-to-responsive animate-box" data-animate-effect="fadeInLeft" >
+						<img src="images/testimony.jpg" alt="" height="500" width="700">
+				</div>
+				<div class="col-md-7 animate-box" data-animate-effect="fadeInRight">
+					<blockquote>
+						<p> &ldquo; "Pure beef talaga yung patty, hindi ginamitan ng extender
+							kaya siksik na siksik. Matitikman mo talaga yung pagka-smokey flavor nya and super sulit!" </p>
+						<p class="author"><cite>&mdash; -Mr. Yowi & Ms. Kathlyn </cite></p>
+						<p class="author"><cite>(Trece Viajero Youtube Vloggers)</cite></p>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-slider" class="fh5co-section animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 animate-box">
+					<div class="fh5co-heading">
+						<h2>The Team behind The craftsmanships</h2>
+				
+					</div>
+				</div>
+				<div class="col-md-6 col-md-push-1 animate-box">
+					<aside id="fh5co-slider-wrwap">
+					<div class="flexslider">
+						<ul class="slides">
+					   	<li style="background-image: url(images/boss.jpg);">
+					   		<div class="overlay-gradient"></div>
+					   		<div class="container-fluid">
+					   			<div class="row">
+						   			<div class="col-md-12 col-md-offset-0 col-md-pull-10 slider-text slider-text-bg">
+						   				<div class="slider-text-inner">
+						   					<div class="desc">
+													<h2>The <em>DC</em> Boss</h2>
+										
+													
+						   					</div>
+						   				</div>
+						   			</div>
+						   		</div>
+					   		</div>
+					   	</li>
+					   	<li stryle="background-image: url(images/designer.jpg);">
+					   		<div class="overlay-gadient"></div>
+					   		<div class="container-fluid">
+					   			<div class="row">
+						   			<div class="col-md-12 col-md-offset-0 col-md-pull-10 slider-text slider-text-bg">
+						   				<div class="slider-text-inner">
+						   					<div class="desc">
+													<h2>The Social Media Manager / Marketing Assistant </h2>
+													<p></p>
+												
+						   					</div>
+						   				</div>
+						   			</div>
+						   		</div>
+					   		</div>
+					   	</li>
+					   	<li style="background-image: url(images/crew.jpg);">
+					   		<div class="overlay-gradient"></div>
+					   		<div class="container-fluid">
+					   			<div class="row">
+						   			<div class="col-md-12 col-md-offset-0 col-md-pull-10 slider-text slider-text-bg">
+						   				<div class="slider-text-inner">
+						   					<div class="desc">
+													<h1></h1>The <em>DC</em> Crew</h1>
+													<h2> “Coming together is a beginning. Keeping together is progress. Working together is success.”
+
+														-Henry Ford </h2>
+												
+						   					</div>
+						   				</div>
+						   			</div>
+						   		</div>
+					   		</div>
+					   	</li>		   	
+					  	</ul>
+				  	</div>
+				</aside>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-slider" class="fh5co-section animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 animate-box">
+					<div class="fh5co-heading">
+						<h2>Get To Know Us More</h2>
+				
+					
+					<p>For more inquiries, bookings and reservations, please message our social media pages by clicking the buttons below.</p>
+					<p> <a href=https://www.facebook.com/dcsteaksandburgers target=_blank class="btn btn-primary btn-outline">Facebook page</a></p>
+					<p> <a href=https://www.instagram.com/dcsteaksandburgers/ target=_blank class="btn btn-primary btn-outline">Instagram page</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h4>DC Steaks & Burgers</h4>
+					<p>May our steaks and burgers bless your apettite.</p>
+				</div>
+				<div class="col-md-2 col-md-push-1 fh5co-widget">
+					<h4>Links</h4>
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Menu</a></li>
+						<li><a href="#">Gallery</a></li>
+					</ul>
+				</div>
+
+
+				<div class="col-md-4 col-md-push-1 fh5co-widget">
+					<h4>Contact Information</h4>
+					<ul class="fh5co-footer-links">
+						<li>Miranda Ext. <br> Brgy. San Nicolas, Angeles City, Pampanga</li>
+						<li><a href="https://www.messenger.com/t/17845435520574680" target=_blank>Click here to message us</a></li>
+					
+					</ul>
+				</div>
+
+			</div>
+
+			
+					</p>
+					<p>
+						<ul class="all rights reserved">
+							<li>ALL RIGHTS RESERVED</li>
+						
+				
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
